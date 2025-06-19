4aa58b71f2495af801c56e7d3ee4102280bdaeab
thirumoolartraining <thirumoolartraining@gmail.com>
2025-06-19 20:03:57 +0530

    fix: make asset paths absolute for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/', // Forces absolute paths for all assets
   plugins: [react()],
   server: {
     port: 3000
